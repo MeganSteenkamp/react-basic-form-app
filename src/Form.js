a29cc651a47470d39c4c742209905787a5bba96b
MeganSteenkamp <megan.steenkamp@gmail.com>
2021-04-13 11:24:23 +1200

    Form updated to use only functional components and useState hook.

--- src/Form.js
+++ src/Form.js
@@ -1,91 +1,90 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-const FormField = ({ id, label, value, errors = {}, handleChange }) => (
-  <>
-    <label htmlFor={id}>{label}</label>
-    <input
-      className={`input ${errors[id] ? 'has-error' : ''}`}
-      type="text"
-      name={id}
-      id={id}
-      value={value}
-      onChange={handleChange}
-    />
-    <span className={`error-message ${id}`}>{errors[id]}</span>
-    <br />
-  </>
-);
-
-class Form extends Component {
-  constructor(props) {
-    super(props);
-
-    this.initialState = {
-      name: '',
-      job: '',
-      errors: {},
-    };
-
-    this.state = this.initialState;
-  }
-
-  handleChange = (event) => {
+/// --- FORM FIELD FUNCTIONAL COMPONENT --- ///
+const FormField = ({ id, label, value, errors = {}, setState }) => {
+  const handleChange = (event) => {
     const { name, value } = event.target;
 
-    this.setState({
+    setState((prevState) => ({
+      ...prevState,
       [name]: value,
-    });
+    }));
   };
 
-  handleValidation() {
-    let formIsValid = true;
-    let errors = {};
+  return (
+    <>
+      <label htmlFor={id}>{label}</label>
+      <input
+        className={`input ${errors[id] ? 'has-error' : ''}`}
+        type="text"
+        name={id}
+        id={id}
+        value={value}
+        onChange={handleChange}
+      />
+      <span className={`error-message ${id}`}>{errors[id]}</span>
+      <br />
+    </>
+  );
+};
 
-    if (this.state.name === '') {
+/// --- FORM FUNCTIONAL COMPONENT --- ///
+const Form = ({ handleSubmit }) => {
+  const initialState = {
+    name: '',
+    job: '',
+    errors: {},
+  };
+
+  const [state, setState] = useState(initialState);
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    
+    let errors = {};
+    let formIsValid = true;
+
+    if (state.name === '') {
       formIsValid = false;
       errors['name'] = 'Enter a name.';
     }
 
-    if (this.state.job === '') {
+    if (state.job === '') {
       formIsValid = false;
       errors['job'] = 'Enter a job.';
     }
 
-    this.setState({ errors: errors });
-
-    return formIsValid;
-  }
-
-  onFormSubmit = (event) => {
-    event.preventDefault();
-
-    if (this.handleValidation()) {
-      this.props.handleSubmit(this.state);
-      this.setState(this.initialState);
+    if (formIsValid) {
+      handleSubmit(state);
+      setState(initialState);
+    } else {
+      setState((prevState) => ({
+        ...prevState,
+        errors,
+        formIsValid,
+      }));
     }
   };
 
-  render() {
-    return (
-      <form onSubmit={this.onFormSubmit}>
-        <FormField
-          id="name"
-          label="Name"
-          value={this.state.name}
-          errors={this.state.errors}
-          handleChange={this.handleChange}
-        />
-        <FormField
-          id="job"
-          label="Job"
-          value={this.state.job}
-          errors={this.state.errors}
-          handleChange={this.handleChange}
-        />
-        <button type="submit">Submit</button>
-      </form>
-    );
-  }
-}
+  return (
+    <form onSubmit={onFormSubmit}>
+      <FormField
+        id="name"
+        label="Name"
+        value={state.name}
+        errors={state.errors}
+        setState={setState}
+      />
+      <FormField
+        id="job"
+        label="Job"
+        value={state.job}
+        errors={state.errors}
+        setState={setState}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
 
 export default Form;
