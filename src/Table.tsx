0ffdb7a841da19631b1cbded74a939d13aa6f911
Megan Steenkamp <megan.steenkamp@numerik.ly>
2021-04-14 08:34:30 +1200

    Table updated to TypeScript. Package manager changed to yarn due to package hoisting issues.

--- src/Table.tsx
+++ src/Table.tsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import { ICharacter } from './App';
+
+interface IProperties {
+  characterData: Array<ICharacter>;
+  removeCharacter: (index: number) => void;
+}
 
 const TableHeader = () => {
   return (
@@ -12,7 +18,7 @@ const TableHeader = () => {
   );
 };
 
-const TableBody = ({ characterData, removeCharacter }) => {
+const TableBody = ({ characterData, removeCharacter }: IProperties) => {
   const rows = characterData.map((row, index) => {
     return (
       <tr key={index}>
@@ -28,7 +34,7 @@ const TableBody = ({ characterData, removeCharacter }) => {
   return <tbody>{rows}</tbody>;
 };
 
-const Table = ({ characterData, removeCharacter }) => {
+const Table = ({ characterData, removeCharacter }: IProperties) => {
   return (
     <table>
       <TableHeader />
